6fd048c524e88ec73f57a0ab18e4a2ca1d93f6ae
Anurag Shrestha <ashrestha@unm.edu>
2024-05-24 19:48:59 -0600

    created a initial scereen to display all watchlist

---
+++ screens/WatchList.tsx
@@ -0,0 +1,8 @@
+import {Text} from 'react-native';
+
+function WatchList() {
+  // eslint-disable-next-line react/react-in-jsx-scope
+  return <Text>This is my watch list.</Text>;
+}
+
+export default WatchList;
