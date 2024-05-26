fed02a4aa1a91c79c810c15832bb943764dd36f6
Anurag Shrestha <ashrestha@unm.edu>
2024-05-26 16:21:46 -0600

    added more properties for cryptoDetails

--- types/navigation.ts
+++ types/navigation.ts
@@ -3,7 +3,16 @@ import type {RouteProp} from '@react-navigation/native';
 export type MainStackParamList = {
   AllCryptoCurrencies: undefined;
   CryptoCurrencies: undefined;
-  CryptoDetails: {id: string};
+  CryptoDetails: {
+    id: string;
+    name: string;
+    symbol: string;
+    price_1hr: string;
+    price_1day: string;
+    price_7day: string;
+    market_cap: string;
+    price: string;
+  };
   WatchList: undefined;
 };
 
