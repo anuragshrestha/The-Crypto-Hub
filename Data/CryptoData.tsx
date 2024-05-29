a88c9c7f52dc26f6c81487147dd664e18613c1e8
Anurag Shrestha <ashrestha@unm.edu>
2024-05-29 09:52:49 -0600

    added new string type

--- Data/CryptoData.tsx
+++ Data/CryptoData.tsx
@@ -9,9 +9,12 @@ export interface CryptoData {
   nameid: string;
   rank: number;
   price_usd: string;
+  price: string;
   percent_change_24h: string;
   percent_change_1h: string;
   percent_change_7d: string;
+  price_1day: string;
+  price_7day: string;
   price_btc: string;
   market_cap_usd: string;
   volume24: number;
