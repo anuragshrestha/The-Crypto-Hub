4e5263144c5e1a0602f33ce7dced97186b966897
Anurag Shrestha <ashrestha@unm.edu>
2024-05-30 20:30:00 -0600

    added AsyncStorage to keep track of the watchlist items

--- screens/CryptoDetails.tsx
+++ screens/CryptoDetails.tsx
@@ -1,4 +1,4 @@
-import React, {useLayoutEffect} from 'react';
+import React, {useCallback, useEffect, useLayoutEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {List, Text} from 'react-native-paper';
 import {useRoute} from '@react-navigation/native';
@@ -6,6 +6,7 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {DetailsScreenRoute, MainStackParamList} from '../types/navigation';
 import IconButton from '../components/IconButton';
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function CryptoDetails({
   navigation,
@@ -15,8 +16,24 @@ function CryptoDetails({
   const price_7day = parseFloat(route.params.price_7day);
   const price = parseFloat(route.params.price).toFixed(2);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  async function saveList() {
+  const [isBlack, setIsBlack] = useState(false);
+  const cryptoKey = `isBlack_${route.params.symbol}`;
+
+  useEffect(() => {
+    const loadStarState = async () => {
+      try {
+        const savedState = await AsyncStorage.getItem(cryptoKey);
+        if (savedState != null) {
+          setIsBlack(JSON.parse(savedState));
+        }
+      } catch (error) {
+        console.error('Error loading the Asyn storage:', error);
+      }
+    };
+    loadStarState();
+  }, [cryptoKey]);
+
+  const saveList = useCallback(async () => {
     const cryptoData = {
       name: route.params.name,
       symbol: route.params.symbol,
@@ -34,21 +51,40 @@ function CryptoDetails({
     } catch (error) {
       console.error('Error saving to list:', error);
     }
-  }
+  }, [
+    route.params.name,
+    route.params.symbol,
+    route.params.price,
+    price_1day,
+    price_7day,
+  ]);
+
+  const changeColor = useCallback(async () => {
+    try {
+      const newState = !isBlack;
+      setIsBlack(newState);
+      await AsyncStorage.setItem(cryptoKey, JSON.stringify(newState));
+    } catch (error) {
+      console.error('Error saving star state:', error);
+    }
+  }, [cryptoKey, isBlack]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
       // eslint-disable-next-line react/no-unstable-nested-components
       headerRight: () => (
         <IconButton
-          icon="star-outline"
+          icon={isBlack ? 'star' : 'star-outline'}
           size={22}
-          color="white"
-          pressed={saveList}
+          color={isBlack ? 'black' : 'white'}
+          pressed={() => {
+            saveList();
+            changeColor();
+          }}
         />
       ),
     });
-  }, [navigation, saveList]);
+  }, [navigation, saveList, isBlack, changeColor]);
 
   return (
     <View style={styles.view}>
